a9a016c52abf1d3de67468fa95df5d189e37c79c
MAUREL Florian <f.maurel@student.isartdigital.com>
2018-05-02 15:16:38 +0200

    Player 3C V1

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,53 +7,42 @@ public class PlayerScript : MonoBehaviour
 
     #region Variables
 
-    [Header("Variables du joueur")]
+    [Header("Global player setting")]
+
+    public int nPlayer;
+    public GameObject goPunchPlayer;
+
+    [Header("Player movements variables")]
 
     public int playerDirection;
-    public int nPlayer;
-    bool canJump = true;
     public float playerSpeed;
+    public bool canJump;
+
+    [Header("Player Actions variables")]
+
+    public bool canPunch;
+    public bool isCollectable;
+    public bool isCollectibleInHands;
 
     #endregion Variables
 
 
-    // Use this for initialization
-    void Start ()
+    void Start()
     {
+        //------Variables initialization------
+        //Initialisation de la position du poing
+        goPunchPlayer.transform.position = new Vector2(transform.position.x + 0.33f, goPunchPlayer.transform.position.y);
 
-       
+        //Variables d'autorisation de saut et de poing
+        canPunch = true;
+        canJump = true;
     }
 
-	// Update is called once per frame
-	void Update ()
+	void Update()
     {
-        #region Commandes joueurs 
-        if (nPlayer == 1) //Controls P1
-        {
-            if (Input.GetKeyDown(KeyCode.Joystick1Button0) && canJump)
-            {
-                gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 300);
-                //canJump = false;
-            }
+        ControllersPlayer();
 
-            if (Input.GetAxis("P1xAxis") >= 0.80f)
-            {
-                playerDirection = 2;
-                gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            }
-
-            else if (Input.GetAxis("P1xAxis") <= -0.80f)
-            {
-                playerDirection = 1;
-                gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            }
-
-            else
-            {
-                playerDirection = 0;
-            }
-        }
-        #endregion Commandes joueurs
+        PlacementHitboxTaper();
     }
 
     void FixedUpdate()
@@ -62,6 +51,83 @@ public class PlayerScript : MonoBehaviour
 
         transform.position = new Vector2(transform.position.x + playerSpeed, transform.position.y);
 
+    }
+
+
+    //--------Deplacement---------
+
+    void ControllersPlayer()
+    {
+
+        #region Saut
+        //Saut avec A
+        if (Input.GetKeyDown(KeyCode.Joystick1Button0) && canJump)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * 300);
+            canJump = false;
+        }
+        #endregion Saut
+
+        #region Taper
+
+        //Punch avec X
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2) && canPunch)
+        {
+            StartCoroutine(HitboxPunch());
+            StartCoroutine(ResetPunch());
+            canPunch = false;
+        }
+
+        #endregion taper
+
+        #region Collectible
+
+        if (Input.GetKeyDown(KeyCode.Joystick1Button3) && isCollectable && !isCollectibleInHands)
+        {
+            isCollectibleInHands = true;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Joystick1Button3) && isCollectibleInHands)
+        {
+            isCollectibleInHands = false;
+        }
+
+        #endregion
+
+
+        #region Commandes joueurs 
+
+        //Movement P1 : Blob
+        if (nPlayer == 1)
+        {
+            //Right
+            if (Input.GetAxis("P1xAxis") >= 0.80f)
+            {
+                playerDirection = 2;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+
+            //Left
+            else if (Input.GetAxis("P1xAxis") <= -0.80f)
+            {
+                playerDirection = 1;
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+
+            //Ne bouge pas 
+            else
+            {
+                playerDirection = 0;
+            }
+        }
+
+        //Movement P2 : Flan
+
+        //Movement P3 : Ghost
+
+        //Movement P4 : Micheline
+
+        #endregion Commandes joueurs
     }
 
     void Deplacement()
@@ -83,5 +149,77 @@ public class PlayerScript : MonoBehaviour
         {
             playerSpeed = 0.05f;
         }
+    }
+
+
+    //--------Interactions---------
+
+    void PlacementHitboxTaper()
+    {
+        //Left
+        if (playerDirection == 1)
+        {
+            goPunchPlayer.transform.position = new Vector2(transform.position.x + -0.33f, goPunchPlayer.transform.position.y);
+        }
+
+        //Right
+        else if (playerDirection == 2)
+        {
+            goPunchPlayer.transform.position = new Vector2(transform.position.x + 0.33f, goPunchPlayer.transform.position.y);
+        }
+    }
+
+
+    
+    //------Collision------
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    //-------Trigger-------
+
+    void OnTriggerStay2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Collectible")
+        {
+            isCollectable = true;
+        }
+
+        if (isCollectibleInHands)
+        {
+            other.gameObject.transform.position = transform.position;
+        }
+    }
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        if (other.gameObject.tag == "Collectible")
+        {
+            isCollectable = false;
+        }
+    }
+
+
+
+    IEnumerator HitboxPunch()
+    {
+        float hitboxActivationTime = 0.3f;
+
+        goPunchPlayer.SetActive(true);
+        yield return new WaitForSeconds(hitboxActivationTime);
+        goPunchPlayer.SetActive(false);
+    }
+
+    IEnumerator ResetPunch()
+    {
+        float timeBeforePunch = 0.5f;
+
+        yield return new WaitForSeconds(timeBeforePunch);
+        canPunch = true;    
     }
 }
