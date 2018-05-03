17e77c8627e631a27fbbb753ed0d253dd16a23d9
Unknown <voidgrape@gmail.com>
2018-05-03 07:07:52 +0200

    Added first Fire Behaviours
    - Ajout d'un tag Fire
    - Ajout de collider pour les salles
    - Ajout d'un script Room (manque la détection de l'état de la salle par les Weakpoints)
    - Paramétrage de la brique Fire (manque le script)

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+	bool isLit;
+	// Use this for initialization
+	void Start () {
+		isLit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D c){
+		if (c.gameObject.tag == "Fire") {
+			isLit = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c){
+		if (c.gameObject.tag == "Fire") {
+			isLit = false;
+		}
+	}
+}
